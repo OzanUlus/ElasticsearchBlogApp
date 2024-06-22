5b5127142fe594c92599a49afcd72073785d1022
Ozan Ulus <ilkerozanulus@gmail.com>
2024-06-22 15:52:53 +0300

    created repo and service

---
+++ ElasticsearchBlogApp/Repository/BlogRepository.cs
@@ -0,0 +1,27 @@
+﻿using Elastic.Clients.Elasticsearch;
+using ElasticsearchBlogApp.Models;
+
+namespace ElasticsearchBlogApp.Repository
+{
+    public class BlogRepository
+    {
+        private readonly ElasticsearchClient _elasticsearchClient;
+        private string indexName = "blog";
+
+        public BlogRepository(ElasticsearchClient elasticsearchClient)
+        {
+            _elasticsearchClient = elasticsearchClient;
+        }
+
+        public async Task<Blog?> SaveAsync(Blog newBlog) 
+        {
+          newBlog.Created = DateTime.Now;
+            var response = await _elasticsearchClient.IndexAsync(newBlog, b =>b.Index(indexName));
+            if (!response.IsValidResponse) return null;
+            newBlog.Id = response.Id;
+            return newBlog;
+            
+        
+        }
+    }
+}
---
+++ ElasticsearchBlogApp/Services/BlogService.cs
@@ -0,0 +1,31 @@
+﻿using ElasticsearchBlogApp.Models;
+using ElasticsearchBlogApp.Repository;
+using ElasticsearchBlogApp.ViewModel;
+
+namespace ElasticsearchBlogApp.Services
+{
+    public class BlogService
+    {
+        private readonly BlogRepository _blogRepository;
+
+        public BlogService(BlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<bool> SaveAsync(BlogCreateViewModel model) 
+        {
+          var newBlog = new Blog 
+          {
+              Title = model.Title,
+              Content = model.Content,
+              UserId = new Guid(),
+              Tags = model.Tags.ToArray(),
+              
+          };
+            var isCreatedBlog = await _blogRepository.SaveAsync(newBlog);
+            return isCreatedBlog != null;
+        
+        }
+    }
+}
